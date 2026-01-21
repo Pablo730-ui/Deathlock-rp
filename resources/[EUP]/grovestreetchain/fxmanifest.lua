a86edd93d97e9bd80f54ab21f7be6dcaa3014513
DaddyDankz420 <chevyboi789@gmail.com>
2026-01-21 12:25:47 -0600

    .

---
+++ resources/[EUP]/grovestreetchain/fxmanifest.lua
@@ -0,0 +1,6 @@
+
+fx_version   'cerulean'
+game         'gta5'
+name         'Grove Street Chain'
+author       'Quimii'
+version      '1.0.0'
--- resources/[maps]/gabz-pdm/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version "cerulean"
-games { 'gta5' }
-
-author 'GTALeak'
-description 'Gabz Hub'
-version '1.0.0'
-
-this_is_a_map 'yes'
