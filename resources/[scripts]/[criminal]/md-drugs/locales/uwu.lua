d618cd2d79821ace698f0ab5dfdb9d5a3ced4582
Pablo730-ui <collinskc0920@gmail.com>
2026-01-28 16:42:54 -0500

    dlrp drugs

---
+++ resources/[scripts]/[criminal]/md-drugs/locales/uwu.lua
@@ -0,0 +1,267 @@
+Lang = {}
+
+Lang.Catches = {
+    notIn = 'Nya~ You\'re not in the right spot! >_<',
+    needItems = 'Aww... you don’t have all da items I need... 😢',
+    notEnoughMoney = 'Oof, you’re too broke, sweetie 💸',
+    buySuccess = 'Yay! You bought %s %s for $%s! 💖',
+    itemMissings = 'You need:%s',
+    itemNeeded = 'You need %s... plz go get it! 🥺',
+    cooldown = 'Silly baka, slow down! ⏳',
+    invalidItem = 'Eh? This item isn’t sold here, silly! 🙃',
+    onCooldownWarn = '%s tried to run %s but is on cooldown, oopsie! ❄️',
+    noCops = 'Nosa! Not enough cops online, okeeday!',
+}
+
+Lang.coke = {
+    targetPick = 'Pick a coca leef (so fresh! 🌿)',
+    picking = 'Picking leefs... so crunchy~ 💚',
+    targetMakePow = 'Make pow-pow from leefs 💨',
+    noleaf = 'Nooo leef? No pow! 😿',
+    makePow = 'Choppin’ leefs into fine pow~ ✂️✨',
+    targetCutCoke = 'Cut da coke, cut it real good ✂️💎',
+    targetBagCoke = 'Bag it up, pretty lil’ bags! 🎀',
+    teleIn = 'Enter the secret lab~ 🔐',
+    teleOut = 'Leave the lab... for now... 👋',
+    needBakingSoda = 'You need bakie soda, silly! 🧂',
+}
+
+Lang.Cornerselling = {
+    inVeh = 'No sellin’ from da car, weirdo! 🚗❌',
+    selling = 'Selling %s for $%s... cha-ching! 💵',
+    deny = 'NOPE! I don’t like dis offer! 🙅‍♀️',
+    wait = 'Waiting for a cutie customer... 👀',
+    nodrugs = 'You got no drugs to sell... 😔',
+    searching = 'Looking for lil’ customers nearby... 🔍',
+    targetTakeBack = 'Take back my stuff! 🫵',
+    targetSell = 'Sell %s of %s for $%s 💼',
+    targetDeny = 'Say no to dumb offers! 🚫',
+    stopped = 'You gave up?? Booo! 😤',
+    start = 'Go make mama proud, cutie! 💪',
+    gotRobbed = 'Owie... you got robbed... 💔',
+    gotBack = 'Yay! You got your stuff back! 🎉',
+    notEnough = 'You don’t have enough, silly! 🥺',
+    comDes = 'Sell drugs on da corner, hustler~ 🕶️',
+}
+
+Lang.crack = {
+    targetMake = 'Cook some crack, bby 💣',
+    cook = 'Cookin’ crack... smoky smoky~ 💨',
+    targetBag = 'Bag da crack, make it purrty~ 🎀',
+    bag = 'Baggin’ crack like a pro! 💼',
+}
+
+Lang.Deliveries = {
+    targetGetDel = 'Get a delivery mission, go go! 🚚',
+    targetOpenShop = 'Open your lil’ shop! 🏪',
+    shopHeader = 'Deally Dealer Shop 💊',
+    targetHandOff = 'Hand off da package, quick! 📦',
+    delivering = 'Givin’ da package to da dealer... 💌',
+    alreadyDelivering = 'You’re already deliverin’, silly! 🐢',
+    slowAf = 'Tooo slow! Mission failed... 😵',
+    newDealerHelp = {
+        help = 'Place a new dealer friend! 🤝',
+        name = 'What’s their name?',
+        timeMin = 'Min time (mins)',
+        timeMax = 'Max time (mins)',
+    },
+    DealerExists = 'Oopsie! Dealer already here! 🙈',
+    deleteDealer = {
+        help = 'Delete a dealer... bye bye~ 👋',
+        name = 'Dealer name to delete?',
+    },
+    deleteDealerSuccess = 'Dealer deleted! Peace out! ✌️',
+    deleteDealerFail = 'Couldn’t delete... maybe they ran? 🏃‍♂️',
+}
+
+Lang.heroin = {
+    targetPick = 'Pick up da plant, bby~ 🌱',
+    picking = 'Picking plant... so fresh! 💚',
+    targetCook = 'Cook da heroin, make it strong 💪',
+    pbCook = 'Cookin’ up some strong stuff... 🔥',
+    targetPickup = 'Pick up lab table~ 🧪',
+    pbPickup = 'Picking up da lil’ lab... 🧫',
+    targetClean = 'Clean da lab, make it sparkly ✨',
+    pbClean = 'Wiping down da table... 🧼',
+    targetDry = 'Dry da plant, make it cruncky~ 💤',
+    pbDry = 'Drying da plant... patience, bby~ ⏳',
+    targetCutHeroin = 'Cut da heroin, make it pretty ✂️',
+    pbCutHeroin = 'Cutting like a pro chef! 👩‍🍳',
+    targetBuyKit = 'Buy a heroin lab kit? 💰',
+    pbBuyKit = 'Buying da kit... now you’re official! 🎉',
+    tableout = 'You already got a table out, silly! 🙃',
+    placing = 'Placing da table... science time! 🔬',
+    targetFill = 'Fill da syringe... be careful! 💉',
+    pbFill = 'Filling da needle... drip drip~ 💧',
+    hasKitAlready = 'You already got a kit, silly! 🤪',
+    notEnoughMoney = 'You need $%s in cash, cutie 💸',
+    placedKit = 'You placed your lab kit! Science go! 🧪✨',
+    failed = 'Oof... you failed and lost it all... 😭',
+}
+
+Lang.lean = {
+    stealFromTruck = 'Steal from da truck, shhh~ 🤫',
+    startMission = 'Start da lean mission! 🍬',
+    notOnRun = 'You’re not on a run, silly! 🙅‍♂️',
+}
+
+Lang.lsd = {
+    targetHeat = 'Heat da LSD, make it bubble~ 🔥',
+    heating = 'Heating up... don’t burn it! 😳',
+    targetRefine = 'Refine da acid, make it pure 💎',
+    refining = 'Purifying da tabs... so shiny~ ✨',
+    targetDipping = 'Dip da tabs, make em glow 💫',
+    dipping = 'Dipping tabs like a cookie~ 🍪',
+    targetPickup = 'Pick up da lab kit~ 🎒',
+    packup = 'Packing up da lab... bye bye~ 👋',
+    targetClean = 'Clean da lab, make it sparkly ✨',
+    needCleaning = 'You need a cleaning kit, silly! 🧽',
+    cleaning = 'Cleaning da lab... so tidy~ 🧹',
+    targetLys = 'Steal lysergic acid... shhh~ 🤫',
+    stealLys = 'Snatching da acid... so sneaky~ 🦝',
+    targetDie = 'Steal diethylamide... risky! 😈',
+    stealDie = 'Grabbin’ da chems... ooooh~ 🔬',
+    targetBuyTab = 'Buy tab paper, make it pretty~ 📄',
+    targetBuyKit = 'Buy LSD lab kit? 💖',
+    alreadyOwn = 'You already got it, silly! 🙃',
+    buyingKit = 'Buying da kit... artist in da making! 🎨',
+    tableout = 'Table already out, no space! 🙈',
+    placing = 'Placing da lab down! Science time~ 🔬',
+    failed = 'You suck at making drugs... lol 😂',
+}
+
+Lang.mesc = {
+    targetPick = 'Pick da cactus, ouchie~ 🌵',
+    picking = 'Picking cactus... careful of spikes! 😬',
+    targetDry = 'Dry da cactus, make it powdery~ 💨',
+    dry = 'Drying slowly... patience, bby~ ⏳',
+}
+
+Lang.meth = {
+    alreadyChasing = 'You’re already chasin’ a truck, silly! 🚛',
+    targetStealFromCar = 'Steal from da meth car~ 🚗',
+    targetHeist = 'Start da meth mission! 💥',
+    started = 'Meth already cookin’, chill! 🧊',
+    increaseHeat = 'You turned up da heat... 🔥',
+    targetBag = 'Bag da meth, make it pretty~ 🎀',
+    targetCook = 'Cook da meth, make it strong 💪',
+    targetGrabTray = 'Grab da meth tray~ 🍽️',
+    targetHeat = 'Adjust da dials... science! 🔬',
+    targetSmash = 'Smash da tray, big baddie~ 💣',
+    targetStealEPH = 'Steal ephedrine... shhh~ 🤫',
+    stealingEPH = 'Snatching da ephedrine... 🧪',
+    targetStealACE = 'Steal acetone... 💨',
+    stealingACE = 'Grabbin’ da acetone... 🧫',
+    notOnRun = 'You’re not on a run, silly! 🙅‍♂️',
+    onCooldown = 'You’re on cooldown, take a nap~ 😴',
+    teleIn = 'Enter da lab... secretly~ 🔐',
+    teleOut = 'Leave da lab... for now~ 👋',
+}
+
+Lang.oxy = {
+    ditchCar = 'Da van is hot! Ditch it quick! 🚨',
+    targetHandoff = 'Hand off da package, fast! 📦',
+    handingoff = 'Givin’ da package... done! ✅',
+    emptyHands = 'Your hands are empty, silly! 🙃',
+    targetPay = 'Rent da oxy van? 💰',
+    keys = 'Keys are in da van... shhh~ 🔑',
+    targetGetPackage = 'Grab da package~ 📦',
+    alreadyCarrying = 'You already got a package! 🤸‍♂️',
+    paid = 'You paid $%s for da van! 💸',
+    notOn = 'You’re not on a route, silly! 🗺️',
+}
+
+Lang.pharma = {
+    input = {
+        header = 'Who gets da prescription, bby?',
+        title1 = 'Player name?',
+        title2 = 'What medicine?',
+    },
+    write = 'Writing da prescription... 📝',
+    opening = 'Opening da bottle... pop! 💊',
+    targetFill = 'Fill da prescription~ 💊',
+    fill = 'Getting it filled... almost! 💉',
+    canceled = 'You canceled... no meds for you! 😢',
+    unbottle = 'You opened da bottle! Yay! 🎉',
+}
+
+Lang.shrooms = {
+    targetPick = 'Pick da shrooms, little fun-gi~ 🍄',
+    picking = 'Picking shrooms... so cute! 💚',
+    eat = 'Eating shrooms... trip time! 🌈',
+    cancel = 'Changed your mind? No tripping! 😌',
+    took = 'You took some shrooms! Wheee~ 🌀',
+}
+
+Lang.weed = {
+    targetPick = 'Pick da weed, so green~ 🌿',
+    picking = 'Picking weed... sticky icky~ ✂️',
+    targetDry = 'Dry da weed, make it crisp~ 💨',
+    noDry = 'You need wet weed, silly! 🌱',
+    wait = 'Waiting for it to dry... patience~ ⏳',
+    take = 'It’s ready! Take it now! 🎉',
+    targetDryed = 'Pick up da dry weed~ 🌿',
+    teleportIn = 'Enter da grow house~ 🏡',
+    teleportOut = 'Leave da grow house~ 👋',
+    dabs = 'Doing some dabs... strong stuff~ 💨',
+    bluntWrap = 'Dipping syrup to make %s... 🍯',
+    rollBlunt = 'Rolling a %s... puffer puffer~ 🚬',
+    targetSales = 'Buy from da dealer~ 💵',
+    buying = 'Buying %s from dealer... 🛒',
+    itemDoesntExist = 'That item doesn’t exist here... 🤷‍♂️',
+    needItem = 'You need a %s for this, silly! 🙃',
+    crackBlunt = 'Cracking open a blunt... 🚬💥',
+}
+
+Lang.wholesale = {
+    alreadyWholesale = 'Already wholesaling, chill! 😌',
+    targetBuyer = 'Talk to da buyer, make a deal~ 💼',
+    wholesaling = 'Wholesaling drugs... big moves~ 💰',
+    uhoh = 'The buyer looks sketchy... 👀',
+    tooLong = 'Buyer got impatient... bye bye~ 👋',
+    notEnoughCops = 'Not enough cops on duty... safe! 🚔',
+    count = 'Found %s itemies~',
+    bonus = 'Bonus: +%s%% mowe sparkwes~ ✨',
+    estimate = 'You get $%s - $%s monies~ 💖',
+    no_drugs = 'No dwugies to seww, sadge~',
+    ambush = 'AMBUSCH! %s bad guys wif %s bang-bangs~ 💥',
+    complete = 'Sowd %s itemies fow $%s%s~ 🎉',
+    min_quantity = 'Need to seww %s itemies min~',
+    deal_failed_cleanup = 'Deaw faiwed... twy again? 💔'
+}
+
+Lang.xtc = {
+    alreadyOut = 'You already got a press out! 🙃',
+    settingDown = 'Setting down da press... 💤',
+    noPressOut = 'No press out... where it go? 🤔',
+    targetMake = 'Press pills, make em pop~ 💊',
+    making = 'Pressing a %s... so satisfying~ ✨',
+    targetPickup = 'Pick up da press~ 🧰',
+    picking = 'Picking up da press... bye bye~ 👋',
+    targetStealMDP2P = 'Steal MDP2P... shhh~ 🤫',
+    stealingMDP2P = 'Snatching da MDP2P... 🧪',
+    fail = 'Damn dude... you really suck... 😅',
+    targetStealIsosafrole = 'Steal isosafrole~ 💨',
+    stealingIsosafrole = 'Grabbin’ da chems... 🧫',
+    targetRaw = 'Make raw XTC, yikes~ 💣',
+    makingRaw = 'Making raw XTC... risky! 🔥',
+    getPress = 'Get press, make pills~ 🖨️',
+    buySingle = 'Buy single press for $%s? 💰',
+    buyp = 'Buying press... factory time! 🏭',
+    targetStamp = 'Stamp XTC, make it pretty~ ✨',
+    stamp_desc = 'Stamp %s pills with design~ 🖋️',
+    stamping = 'Stamping %s pills... so clean~ ✨',
+    noPills = 'You got no unstamped pills... 🫙',
+}
+
+Lang.merchant = {
+    targetOpen = 'Open Wopen Store',
+}
+
+Lang.Whippit = {
+    targetShop = 'Open Whippit Shop',
+    needBalloon = 'You need a balloon for this, cutie! 🎈',
+    needCanister = 'You need a canister for this, bby! 🛢️',
+    shopHeader = 'Whippit Shop 🥳',
+}
+return Lang
