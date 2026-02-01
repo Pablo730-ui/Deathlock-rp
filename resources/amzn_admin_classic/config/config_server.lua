2a534f21cb90db268a422832f2278af0c899e758
Pablo730-ui <collinskc0920@gmail.com>
2026-01-31 20:43:23 -0500

    fs

---
+++ resources/amzn_admin_classic/config/config_server.lua
@@ -0,0 +1,9 @@
+Config                                      = Config or {}  -- Don't touch!
+
+-- Logs/Screenshot feature Config
+-- Set the discord webhook for the logs and screenshots.
+Config.LogsWebhook                          = ""
+Config.ScreenshotWebhook                    = ""
+
+-- Set this to your discord bot token for the discord roles integration. This is not required for anything else.
+Config.DiscordRolesBotToken = ""
